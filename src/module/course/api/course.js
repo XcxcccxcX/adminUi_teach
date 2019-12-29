22b9c0f1de7c7a7b298a37de47def7f9d7b84920
xcx <15988418117@163.com>
2019-12-29 20:04:54 +0800

    课程添加,课程修改,课程营销

--- src/module/course/api/course.js
+++ src/module/course/api/course.js
@@ -19,13 +19,29 @@ export const category_findlist= () => {
 export const addCourseBase = params => {
   return http.requestPost(apiUrl+'/course/coursebase/add',params)
 }
+/*查询课程信息*/
+export const getCoursebaseById = courseBaseId => {
+  return http.requestQuickGet(apiUrl+'/course/coursebase/get/'+courseBaseId)
+}
 /*查询课程计划*/
 export const findTeachplanList = courseid => {
   return http.requestQuickGet(apiUrl+'/course/teachplan/list/'+courseid)
 }
+/*修改课程基本信息*/
+export const updateCoursebase = (courseId,params) => {
+  return http.requestPost(apiUrl+'/course/coursebase/update/'+courseId,params)
+}
 /*添加课程计划*/
 export const addTeachplan = teachplah => {
   return http.requestPost(apiUrl+'/course/teachplan/add',teachplah)
+}
+/*获取课程营销信息*/
+export const getCourseMarketById = courseid => {
+  return http.requestQuickGet(apiUrl+'/course/coursemarket/get/'+courseid)
+}
+/*修改/添加 课程营销信息*/
+export const updateCourseMarket = (courseid,params) => {
+  return http.requestPost(apiUrl+'/course/coursemarket/update/'+courseid,params)
 }
 
 //保存课程图片地址到课程数据 库
